4ad6ab3f94c170557b6ea78266be21e337ce85d8
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-05 19:49:14 -0300

    atualização

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
+//Seleção dos icones
+let menuItem = document.querySelectorAll('.item-menu')
 
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=> 
+    item.addEventListener('click', selectLink)
+)
+
+//Expandir menu
+
+let btnExp = document.querySelector('#expandir')
+let menu = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click' , function(){
+    menu.classList.toggle('expandir')
+})
